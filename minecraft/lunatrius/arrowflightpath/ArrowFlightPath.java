5eaf9fab7b21c70eab12f9f8821ac3f41e71aff3
Lunatrius <jadran.kotnik@gmail.com>
2013-12-13 21:42:07 +0100

    And so it begins... :)

---
+++ minecraft/lunatrius/arrowflightpath/ArrowFlightPath.java
@@ -0,0 +1,20 @@
+package lunatrius.arrowflightpath;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+import static cpw.mods.fml.common.Mod.EventHandler;
+
+@Mod(modid = "ArrowFlightPath")
+public class ArrowFlightPath {
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+}
